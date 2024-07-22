b0c59de4e25dd936f91237347d2526261ee63201
BiTech5 <bikikc7@gmail.com>
2024-07-22 19:39:20 +0545

    making serializers

---
+++ apps/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Post, Comment
+
+
+#making serializers for Post and Comment model
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Post
+        fields='__all__'
+        
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Comment
+        fields='__all__'

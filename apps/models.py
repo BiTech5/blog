778836d586022d20b2e8432c36d9ed978114223c
BiTech5 <bikikc7@gmail.com>
2024-07-22 19:28:50 +0545

    making models

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=200)
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    content=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    post=models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    content=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
